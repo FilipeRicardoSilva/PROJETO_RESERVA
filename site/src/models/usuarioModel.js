59ee88e1cbbdf484f7baa704d89e02d4cea77305
FilipeRicardoSilva <110927607+FilipeRicardoSilva@users.noreply.github.com>
2022-11-29 17:10:10 -0300

    API COMPLETA
    API COMPLETA

---
+++ site/src/models/usuarioModel.js
@@ -0,0 +1,59 @@
+var database = require("../database/config")
+
+function listar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM usuario;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function entrar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function exibirUsuario(idUsuario) {
+    var instrucao = `
+        select round((avg(totalPontos)), 2) as 'metrica' u.nome from usuario u join quiz q on u.idUsuario = q.fkUsuario where idUsuario = ${idUsuario} group by fkUsuario; 
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function cadastrar(nome, email, senha, confirmacaoSenha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, confirmacaoSenha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO usuario (nome, email, senha, confirmacaoSenha) VALUES ('${nome}', '${email}', '${senha}', 
+        '${confirmacaoSenha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function cadastrarPontos(total, fkUsuario) {
+    var instrucao = `
+        INSERT INTO quiz (totalPontos, fkUsuario) VALUES (${total}, ${fkUsuario});
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    cadastrarPontos,
+    listar,
+    exibirUsuario
+};
